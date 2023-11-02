f03abcd0c60fce5d48c7ca4deb0688f053eebc73
Muying Zhao <muying.zhao@postgrad.manchester.ac.uk>
2023-11-02 16:38:47 +0000

    add 1~2 functions

--- app/views/controllers.py
+++ app/views/controllers.py
@@ -42,7 +42,7 @@ def home():
 
 def generate_data_for_tiles():
     """Generate the data for the four home page titles."""
-    return [db_mod.get_total_number_items()]
+    return [db_mod.get_total_number_items(), db_mod.get_average_ACT_cost(), db_mod.get_number_of_unique_items()]
 
 def generate_barchart_data():
     """Generate the data needed to populate the barchart."""
