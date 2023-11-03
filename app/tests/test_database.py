cdd8088433b8c0acea6c4115e8550f6e1fd54b85
Wenxin Zhang <1652659159@qq.com>
2023-11-03 19:09:58 +0800

    Finished the test about correctness of our calculated value and finished the test about coverage about our database

--- app/tests/test_database.py
+++ app/tests/test_database.py
@@ -8,8 +8,12 @@ DESCRIPTION:   Suite of tests for testing the dashboards database
                functionality.
 """
 
+
 import unittest
-#from app import app
+import sys
+sys.path.append('D:\\miekeleton\\mie_G14')
+
+from app import app
 from app.database.controllers import Database
 
 class DatabaseTests(unittest.TestCase):
@@ -23,8 +27,29 @@ class DatabaseTests(unittest.TestCase):
         pass
 
     def test_get_total_number_items(self):
-        """Test that the total number of items returns the correct value."""
-        self.assertEquals(self.db_mod.get_total_number_items(), 8218165, 'Test total items returns correct value')
+        """Test that the total number of items."""
+        with app.app_context():
+            self.assertEquals(self.db_mod.get_total_number_items(), 8218165, 'Test total items returns correct value')
+            self.assertFalse(self.db_mod.get_total_number_items(), 'Test total items returns incorrect value')
+    def test_average_ACT_cost(self):
+        """Test that the average ACT cost."""
+        with app.app_context():
+            self.assertEquals(self.db_mod.get_average_ACT_cost(), 76.22, 'Test average ACT cost returns correct value')
+            self.assertFalse(self.db_mod.get_average_ACT_cost(), 'Test average ACT cost returns incorrect value')
+    def test_get_TOP_PRESCRIBED_ITEM(self):
+        """Test that top prescribed item."""
+        with app.app_context():
+            self.assertEquals(self.db_mod.get_TOP_PRESCRIBED_ITEM(), 'Methadone HCL_Oral Soln 1mg/1ml S/F', 'Test top prescribed item return correct value')
+            self.assertFalse(self.db_mod.get_TOP_PRESCRIBED_ITEM(), 'Test top prescribed item return incorrect value')
+
+    def test_get_number_of_unique_items(self):
+        """Test that the number of unique items."""
+        with app.app_context():
+            self.assertEquals(self.db_mod.get_number_of_unique_items(), 13935, 'Test number of unique items return correct value')
+            self.assertFalse(self.db_mod.get_number_of_unique_items(), 'Test number of unique items return incorrect value')
+
+
+
 
     def test_average_ACT_cost(self):
         """Test that the average ACT cost returns the correct value."""
